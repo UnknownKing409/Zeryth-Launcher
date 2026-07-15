3b3bc8c94b1549a69c0195cdc51deb258e54c15a
Rami1l <johnrenonasuncion-ramil@users.noreply.github.com>
2026-07-15 02:18:48 +0000

    Fix missing CardTitleLayout import

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/LauncherScreen.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/LauncherScreen.kt
@@ -138,6 +138,7 @@ import com.movtery.zalithlauncher.setting.enums.DashboardMode
 import com.movtery.zalithlauncher.setting.enums.MainScreenMode
 import com.movtery.zalithlauncher.ui.base.BaseScreen
 import com.movtery.zalithlauncher.ui.components.BackgroundCard
+import com.movtery.zalithlauncher.ui.components.CardTitleLayout
 import com.movtery.zalithlauncher.ui.components.MarkdownView
 import com.movtery.zalithlauncher.ui.components.MarqueeText
 import com.movtery.zalithlauncher.ui.components.ScalingActionButton
